d2188c602e2d84a12fbd6f5f2f723cb760534fb4
root <root@vps783093.ovh.net>
2020-03-17 17:54:55 +0100

    Działające okno modalne

---
+++ nutrimngr/app/controllers/modal_controller.rb
@@ -0,0 +1,9 @@
+class ModalController < ActionController::Base
+    
+    def modal
+        respond_to do |format|
+            format.html
+            format.js
+        end
+    end
+end
--- nutrimngr/config/routes.rb
+++ nutrimngr/config/routes.rb
@@ -2,4 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'index/index'
   root 'index#index'
+  get 'modal/modal' => 'modal#modal', :as => :modal
 end
