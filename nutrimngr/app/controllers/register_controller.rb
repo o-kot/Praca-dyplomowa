04227f672e4928ecbd9c14ec074a803195a3390f
root <root@vps783093.ovh.net>
2020-03-23 22:51:16 +0100

    Koniec implementacji 1. iteracji oraz jej opis w pracy.doc

--- nutrimngr/app/controllers/register_controller.rb
+++ nutrimngr/app/controllers/register_controller.rb
@@ -2,6 +2,8 @@ class RegisterController < ApplicationController
     def register
         if params['password'] != params['confirm_password']
             render plain: 'Podane hasła nie są identyczne'
+        elsif params['email'] || params['password'] || params['confirm_password'] == ''
+            render plain: 'Formularz zawiera niewypełnione pola.'
         else
             newUser = UserPg.new
             render plain: newUser.saveUser(params['email'], params['password'])         
